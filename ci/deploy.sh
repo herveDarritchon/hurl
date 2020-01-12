5418056f0f1b92d70b16de6f5cb1cc61b277de31
Fabrice Reix <fabrice.reix@gmail.com>
2020-01-12 22:04:06 +0100

    FIX

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,7 +3,21 @@
 set -e
 set -u
 
-ci/check_tag "$VERSION"
+echo check git tag
+TAG=$(git tag --points-at HEAD |tr -d '\n')
+if [ "$TAG" == "" ]; then
+        echo "Tag is not set"
+        exit 0
+fi
+
+if [ "$TAG" != "$VERSION" ]; then
+        echo "Tag '$TAG' does not match version '$VERSION'"
+        exit 1
+fi
+echo tag matches version
+
 ci/create_tarballs target/tarballs
 ci/upload.sh "$VERSION" "target/tarballs/hurl-$VERSION-x86_64-linux.tar.gz"
+
+
 
